1c21784a44ef18e49da171d36ee01f78a2ef0381
Joseph Gentle <josephg@gmail.com>
2013-04-05 11:26:31 -0700

    Added initial plugin detection

--- Controller360/main.m
+++ Controller360/main.m
@@ -7,16 +7,49 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <IOKit/usb/IOUSBLib.h>
 
-int main(int argc, const char * argv[])
-{
+#define PRODUCT_ID 0x028e // xbox360 controller
+#define VENDOR_ID 0x045e // microsoft
+#define EXPECTED_VERSION 0x0114
+
+void pump(io_iterator_t iter) {
+  io_service_t ref;
+  while ((ref = IOIteratorNext(iter))) {
+    NSLog(@"Gamepad connected");
+  }
+}
+
+void gamepadConnected(void *unused, io_iterator_t iter) {
+  NSLog(@"hi");
+  pump(iter);
+}
+
+int main(int argc, const char * argv[]) {
 
   @autoreleasepool {
-      
-      // insert code here...
-      NSLog(@"Hello, World!");
-      
+    CFMutableDictionaryRef matchingDict = IOServiceMatching(kIOUSBDeviceClassName);
+    CFDictionaryAddValue(matchingDict, CFSTR(kUSBVendorID), [[NSNumber numberWithInt:VENDOR_ID] autorelease]);
+    CFDictionaryAddValue(matchingDict, CFSTR(kUSBProductID), [[NSNumber numberWithInt:PRODUCT_ID] autorelease]);
+    
+//    IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &iter);
+    
+    IONotificationPortRef port = IONotificationPortCreate(kIOMasterPortDefault);
+    CFRunLoopSourceRef source = IONotificationPortGetRunLoopSource(port);
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
+
+    //    IONotificationPortRef port = IONotificationPortCreate(CFRunLoopGetMain());
+    io_iterator_t iter;
+    
+    IOReturn ret = IOServiceAddMatchingNotification(port, kIOFirstMatchNotification, matchingDict, gamepadConnected, NULL, &iter);
+    if (ret != kIOReturnSuccess) {
+      NSLog(@"error: %d", ret);
+    }
+    
+    pump(iter);
+    
+    CFRunLoopRun();
   }
-    return 0;
+  return 0;
 }
 
