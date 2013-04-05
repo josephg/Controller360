e552f78208ea7b26f8cf9e0198a3d09fd92a795c
Joseph Gentle <josephg@gmail.com>
2013-04-04 18:27:53 -0700

    Initial Commit

---
+++ Controller360/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Controller360
+//
+//  Created by Joseph Gentle on 4/04/13.
+//  Copyright (c) 2013 Joseph Gentle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
